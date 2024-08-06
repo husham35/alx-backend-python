0309030199d2f352f9e8742eb70a89741850113b
husham35 <husham35@gmail.com>
2024-08-06 17:53:24 +0000

    task 2 fixes again

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -16,7 +16,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     Returns `total_time / n`
     """
